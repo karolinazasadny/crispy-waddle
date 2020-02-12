c457348565220a597fd4d797022f93b943cda6e6
karolinazasadny <zasadnykarolina@gmai.com>
2020-02-12 18:19:30 +0100

    creating secants

--- generalizacja.py
+++ generalizacja.py
@@ -31,33 +31,42 @@ def DataImport ():
 
     buildings = []
     for row in arcpy.da.SearchCursor(data, ["OID@", "SHAPE@"]):
+        parts = []
         for part in row[1]:
             vertices = []
             for i in part:
                 vertices.append((i.X, i.Y))
-        buildings.append(vertices)
+            parts.append(vertices)
+        buildings.append(parts)
     return buildings
 
 def Simplify(buildings):
 
     build_simpl = []
     for build in buildings:
-        vert = 0
         vert_simpl = []
-        for pnt in build:
-            if vert == 0:
-                check = Angle(build[-1][0],build[0][0],build[1][0],build[-1][1],build[0][1],build[1][1])
-            elif vert == len(build)-1:
-                check = Angle(build[-2][0],build[-1][0],build[0][0],build[-2][1],build[-1][1],build[0][1])
-            else: check = Angle(build[vert-1][0],build[vert][0],build[vert+1][0],build[vert-1][1],build[vert][1],build[vert+1][1])
+        for part in build:
+            vert = 0
+            for pnt in part:
+                if vert == 0:
+                    check = Angle(part[-1][0],part[0][0],part[1][0],part[-1][1],part[0][1],part[1][1])
+                elif vert == len(part)-1:
+                    check = Angle(part[-2][0],part[-1][0],part[0][0],part[-2][1],part[-1][1],part[0][1])
+                else: check = Angle(part[vert-1][0],part[vert][0],part[vert+1][0],part[vert-1][1],part[vert][1],part[vert+1][1])
 
-            if check <= m.pi-float(tolerance) or check >= m.pi+float(tolerance):
-                vert_simpl.append(pnt)
-            vert+=1
+                if check <= m.pi-float(tolerance) or check >= m.pi+float(tolerance):
+                    vert_simpl.append(pnt)
+                vert+=1
         build_simpl.append(vert_simpl)
     return build_simpl
 
+def Distance(X1,X2,Y1,Y2):
+    
+     dist = m.sqrt((X2-X1)**2+(Y2-Y1)**2)
+     return dist
+
 def CreateSHP(data, file_name):
+    
     shp = arcpy.CreateFeatureclass_management(r".\\", str(file_name)+".shp", "POLYGON")
     cursor = arcpy.da.InsertCursor(shp, ["SHAPE@"])
 
@@ -65,10 +74,33 @@ def CreateSHP(data, file_name):
         cursor.insertRow([build])
     del cursor
 
+def Secant (building):
+
+    secants = []
+    k=2
+    for pnt in building[2:-2]:
+        d = Distance(building[0][0],pnt[0],building[0][1],pnt[1])
+        slow = {'id':(str(0)+str(k)), 'lenght':d, 'vert_num':0}
+        secants.append(slow)
+        k+=1
+
+    i=1
+    for pnt in building[1:]:
+        j=i+2
+        for oth in building[i+2:-1]:
+            d = Distance(pnt[0],oth[0],pnt[1],oth[1])
+            slow = {'id':str(i)+str(j), 'lenght':d, 'vert_num':0}
+            secants.append(slow)
+            j+=1
+        i+=1
+
+    print secants
+    
 
 def main ():
     data = DataImport()
     s_b = Simplify(data)
-    CreateSHP(s_b, "nowe")
+    #CreateSHP(s_b, "nowe")
+    Secant(s_b[4])
     
 main()
