7a8b6c9b32bd60d50475235f2292aa18b8797994
karolinazasadny <zasadnykarolina@gmai.com>
2020-02-16 20:32:02 +0100

    correcting mistakes, adding fields

--- generalizacja.py
+++ generalizacja.py
@@ -4,14 +4,17 @@ import math as m
 
 global tolerance
 global k
+global bud_nr
 
 arcpy.env.overwriteOutput = True
+arcpy.env.qualifiedFieldNames = False
 
-tolerance = float(raw_input ("Podaj tolerancjê upraszczania budynków [rad]: "))
-k = int(raw_input ("Podaj liczbê wierzcho³ków odcinanych przez sieczn¹: "))
+tolerance = float(raw_input ("Podaj tolerancjê upraszczania budynków [stopnie]: "))*m.pi/180
+k = int(raw_input ("Podaj liczbê wierzcho³ków odcinanych przez sieczn¹ (>2): "))
 
-##tolerance = 0.1
-##k = 3
+if k<3: k = int(raw_input ("Podaj liczbê wierzcho³ków odcinanych przez sieczn¹ (>2): "))
+
+bud_nr = 0
 
 def Azimuth (X1,X2,Y1,Y2):
 
@@ -65,27 +68,34 @@ def Distance(X1,X2,Y1,Y2):
      dist = m.sqrt((X2-X1)**2+(Y2-Y1)**2)
      return dist
 
-def CreateSHP(data, file_name):
+def CreateSHP(data, file_name, *fields):
     
     shp = arcpy.CreateFeatureclass_management(r".\\", str(file_name)+".shp", "POLYGON")
-    cursor = arcpy.da.InsertCursor(shp, ["SHAPE@"])
+    col = ["SHAPE@"] + list(fields)
+    cursor = arcpy.da.InsertCursor(shp, col)
+
+    for field in fields:
+        arcpy.AddField_management(shp, field, "Integer")
 
     for build in data:
-        cursor.insertRow([build])
+        cursor.insertRow(build)
+        
     del cursor
 
 def Secant(building):
 
     array = arcpy.Array([arcpy.Point(*coords) for coords in building])
-    build = arcpy.Geometry("POLYGON", array)                      
+    build = arcpy.Geometry("POLYGON", array)
     
     secants = []
     k=2
     for pnt in building[2:-2]:
         d = Distance(building[0][0],pnt[0],building[0][1],pnt[1])
         num = k+1
-        diction = {'id':(str(0)+str(k)), 'lenght':d, 'vert_num':num, 'id_from':0, 'id_to':k}
         geom = arcpy.Geometry("POLYLINE", arcpy.Array([arcpy.Point(building[0][0],building[0][1]), arcpy.Point(pnt[0],pnt[1])]))
+        if geom.within(build): in_out = 1
+        else: in_out = 0
+        diction = {'id':(str(0)+str(k)), 'lenght':d, 'vert_num':num, 'id_from':0, 'id_to':k, "in_out":in_out}
         if not geom.crosses(build):
             secants.append(diction)
         k+=1
@@ -96,8 +106,10 @@ def Secant(building):
         for oth in building[i+2:-1]:
             d = Distance(pnt[0],oth[0],pnt[1],oth[1])
             num = j-i+1
-            diction = {'id':str(i)+str(j), 'lenght':d, 'vert_num':num, 'id_from':i, 'id_to':j}
             geom = arcpy.Geometry("POLYLINE", arcpy.Array([arcpy.Point(pnt[0],pnt[1]), arcpy.Point(oth[0],oth[1])]))
+            if geom.within(build): in_out = 1
+            else: in_out = 0
+            diction = {'id':str(i)+str(j), 'lenght':d, 'vert_num':num, 'id_from':i, 'id_to':j, "in_out":in_out}
             if not geom.crosses(build):
                 secants.append(diction)
             j+=1
@@ -108,30 +120,35 @@ def Secant(building):
 
 def Generalization(secants,building):
 
+    global bud_nr
+    global k
+    
     count = len(building)
     dict_xy = {i:building[i] for i in range (len(building))}
-    xy_rej = []
     rej_draw = []
     xy_check = {}
     remain = []
+    rej = 0
 
     for sec in secants:
         if sec['vert_num'] >= k and sec['id_from'] not in xy_check.keys() and sec['id_to'] not in xy_check.keys():
             i=0
-            count = len(building) - len(xy_check)
-            if count <= 5: break
             xy_rej = []
             for vert in range(sec['id_from'], sec['id_to']+1):
                 if building[vert] not in xy_check.keys():
                     if not i== 0 and not i == sec['id_to']-sec['id_from']:
+                        if len(dict_xy)-len(xy_check) <=5 : break
                         xy_check[vert] = building[vert]
                     xy_rej.append(building[vert])
                     i+=1
-            rej_draw.append(xy_rej)
+            rej_draw.append([xy_rej, bud_nr, rej, sec['in_out']])
+            rej += 1
 
     
     for pnt in dict_xy:
         if pnt not in xy_check: remain.append(dict_xy[pnt])
+
+    bud_nr += 1
 
     return (rej_draw, [remain])
  
@@ -146,9 +163,9 @@ def main ():
 
     for build in result:
         rejected += build[0]
-        remained += build[1]
+        remained += [build[1]]
 
-    CreateSHP(rejected, "rejected")
+    CreateSHP(rejected, "rejected", "id", "id_s", "in_out")
     CreateSHP(remained, "remained")
     
 if __name__ == '__main__':
